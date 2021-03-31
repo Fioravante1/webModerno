807229c34bec4d88e1f487e954e3bb94de272f13
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-31 12:11:09 -0300

    Formas de deixar o this associado a uma funçao

--- thisbind.js
+++ thisbind.js
@@ -11,3 +11,19 @@ falar();
 
 const falarDePessoa = pessoa.falar.bind(pessoa);
 falarDePessoa();
+
+// Continuando com os exemplos de this e bind
+
+function pessoa() {
+  this.idade = 0
+
+  const self = this;
+  setInterval(function() {
+  self.idade++
+  console.log(self.idade);
+  }, 1000);
+};
+
+
+
+new pessoa
