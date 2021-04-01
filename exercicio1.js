d81e4d4530a117b7b40fda37e7c4961c8c201523
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-04-01 14:59:36 -0300

    Exercicio1 sobre funções

---
+++ exercicio1.js
@@ -0,0 +1,8 @@
+const calculadora = (number1, number2) => {
+  return `A soma dos numeros é ${number1 + number2},
+  A subtração dos numeros é ${number1 - number2},
+  A multiplicação dos numeros é ${number1 * number2},
+  E a divisão dos numeros é ${number1 / number2}.`
+};
+
+console.log(calculadora(50, 23));
