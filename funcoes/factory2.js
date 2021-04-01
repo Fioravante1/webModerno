f5e1aaa361f9926b2a6ff87cb2d89e20c156bd29
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-04-01 14:01:34 -0300

    Funcoes factory isso é muito interesante

---
+++ funcoes/factory2.js
@@ -0,0 +1,12 @@
+// Criando uma função fabrica(factory)
+
+function criarProduto(nome, preco) {
+  return {
+    nome,
+    preco,
+    desconto: 0.1
+  };
+};
+
+console.log(criarProduto('Celular', 2199));
+console.log(criarProduto('televisão', 3199));
